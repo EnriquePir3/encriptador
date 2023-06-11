875f5cfe4e37e4f6983c2cae66b01e399ffb0f27
Keyber Pire <keypire1403@gmail.com>
2023-06-10 22:51:23 -0400

    ultima actualizacion, con javascript resuelto

--- script.js
+++ script.js
@@ -1,45 +1,75 @@
-
-
+document.getElementById("area-texto-encriptado").style.display = "none";
+document.getElementById("texto").focus();
 function encriptar(){
-
     let texto = document.getElementById("texto").value;
     let mayusculas = /[A-Z]/;
     let acentos = /[áéíóú]/;
-    let resultado = "prueba";
+    
 
     if (texto == ""){ 
 
-        console.log("no hay un nada manito");
+        alert("ningun mensaje ha sido escrito");
     
     } else if ( 
-            mayusculas.test(document.getElementById("texto").value) ||
+          mayusculas.test(document.getElementById("texto").value) ||
             acentos.test(document.getElementById("texto").value )) {
 
-        console.log("no puedes usar letras mayusculas ni acentos, por favor escribe tu texto tomando en cuenta estas restricciones.Gracias.");
+              alert("no puedes usar letras mayusculas ni acentos, por favor escribe tu texto tomando en cuenta estas restricciones.Gracias.");
 
     } else {
-        resultado = encrypt(texto);
+    let textoEncriptado = encriptarTexto(texto);
+    
+          
+          
+        document.getElementById("contenido-pantalla-encriptado").style.display = "none";
+        
+           document.getElementById("area-texto-encriptado").style.display = "block";
+            
+                document.getElementById("mensaje-encriptado").value = textoEncriptado;
+}
     }
     
-   // Mostrar el área de texto encriptado y asignar el valor encriptado al campo correspondiente
-   document.getElementById("contenido-pantalla-encriptado").style.display =
-   "none";
- document.getElementById("area-texto-encriptado").style.display = "block";
- document.getElementById("mensaje-encriptado").value = resultado;
-}
 
-function encrypt(text) {
-    let encrypted = text.replace(/e/g, "enter");
-    encrypted = encrypted.replace(/i/g, "imes");
-    encrypted = encrypted.replace(/a/g, "ai");
-    encrypted = encrypted.replace(/o/g, "ober");
-    encrypted = encrypted.replace(/u/g, "ufat");
-    return encrypted;
+function desencriptar(){
+    
+    textoEncriptado = document.getElementById("texto").value;
+
+    if (textoEncriptado == ""){
+        alert("no hay nada que descifrar");
+    }
+        
+   let textoDesencriptado = desencriptarTexto(textoEncriptado)
+
+    document.getElementById("mensaje-encriptado").value = textoDesencriptado; 
+
+} 
+
+function encriptarTexto(texto) {
+    let encriptar = texto.replace(/e/g, "enter");
+    encriptar = encriptar.replace(/i/g, "imes");
+    encriptar = encriptar.replace(/a/g, "ai");
+    encriptar = encriptar.replace(/o/g, "ober");
+    encriptar = encriptar.replace(/u/g, "ufat");
+    return encriptar;
   }
 
+  function desencriptarTexto(texto) {
+    let desencriptar = texto.replace(/enter/g, "e");
+    desencriptar = desencriptar.replace(/imes/g, "i");
+    desencriptar = desencriptar.replace(/ai/g, "a");
+    desencriptar = desencriptar.replace(/ober/g, "o");
+    desencriptar = desencriptar.replace(/ufat/g, "u");
+    return desencriptar;
+}
+
+function copiarTexto(idtexto) {
+    let texto = document.getElementById("mensaje-encriptado");
+    texto.select();
+    document.execCommand("copy");
+  }
+
+  
 
 
 
-
-
-
+/* fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!*/
